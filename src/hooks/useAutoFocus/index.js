0963ac728b1678d2df58e23cf9cd46390acbb96a
yehor-akunishnikov <faiteforever@gmail.com>
2023-10-22 05:24:56 +0300

    Added optional chaining to focus hook, to make it more stable

--- src/hooks/useAutoFocus/index.js
+++ src/hooks/useAutoFocus/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const useAutoFocus = (ref, selector) => {
   React.useEffect(() => {
-    ref.current.querySelector(selector).focus();
+    ref.current?.querySelector(selector)?.focus();
   }, []);
 };
